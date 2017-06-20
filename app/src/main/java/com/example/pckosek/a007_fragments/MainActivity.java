012228a611813549259e078d956d2d8e8bd32eef
pckosek <pckosek@local.tjhsst.edu>
2017-06-20 10:06:22 -0400

    v 1.0

---
+++ app/src/main/java/com/example/pckosek/a007_fragments/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.pckosek.a007_fragments;
+
+/* ------------------------*/
+/*    FILE VERSION 1.0     */
+/* ------------------------*/
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+    }
+
+    // A Fragment of type #1
+
+    public static class FragmentOne extends Fragment {
+
+        public FragmentOne() {
+        }
+
+        public static FragmentOne newInstance() {
+            FragmentOne fragment = new FragmentOne();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText("This is a Fragment 1");
+            return rootView;
+        }
+    }
+
+    // A Fragment of type #2
+
+    public static class FragmentTwo extends Fragment {
+
+        public FragmentTwo() {
+        }
+
+        public static FragmentTwo newInstance() {
+            FragmentTwo fragment = new FragmentTwo();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText("This is a Fragment 2");
+            return rootView;
+        }
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0 :
+                    return FragmentOne.newInstance();
+                case 1 :
+                    return FragmentTwo.newInstance();
+                default :
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+    }
+}
